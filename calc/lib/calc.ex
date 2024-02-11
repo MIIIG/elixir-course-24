2a71708ef93b0ff91b0f4e539d3fe50de6dd75a1
Артур Бадретдинов <arthur.badretdinov@gmail.com>
2024-02-11 23:55:22 +0500

    working with the types
    add some type checks, test, and divisiono on zero

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -8,37 +8,88 @@ defmodule Calc do
 
       iex> Calc.add(2, 2)
       4
+
+      iex> Calc.add("2", "2")
+      "Bad argument!"
   """
-  @spec add(integer(), integer()) :: integer()
-  def add(x, y), do: x + y
+  @spec add(integer() | float(), integer() | float()) :: integer() | float()
+  def add(x, y)
+      when is_integer(x) or
+             is_float(x) or
+             is_integer(y) or
+             is_float(y) do
+    x + y
+  end
+
+  def add(_x, _y), do: "Bad argument!"
 
   @doc """
   ## Subtraction
 
     iex> Calc.sub(4, 2)
     2
+
+    iex> Calc.sub("4", "2")
+    "Bad argument!"
   """
 
-  @spec sub(integer(), integer()) :: integer()
-  def sub(x, y), do: x - y
+  @spec sub(integer() | float(), integer() | float()) :: integer() | float()
+  def sub(x, y)
+      when is_integer(x) or
+             is_float(x) or
+             is_integer(y) or
+             is_float(y) do
+    x - y
+  end
+
+  def sub(_x, _y), do: "Bad argument!"
 
   @doc """
   ## Multuply
 
     iex> Calc.multi(3, 5)
     15
+
+    iex> Calc.multi("3", "5")
+    "Bad argument!"
   """
 
-  @spec multi(integer(), integer()) :: integer()
-  def multi(x, y), do: x * y
+  @spec multi(integer() | float(), integer() | float()) :: integer() | float() | :error
+  def multi(x, y)
+      when is_integer(x) or
+             is_float(x) or
+             is_integer(y) or
+             is_float(y) do
+    x * y
+  end
+
+  def multi(_x, _y), do: "Bad argument!"
 
   @doc """
   ## Division
-  
+
     iex> Calc.div(15, 3)
     5.0
+
+    iex> Calc.div(15, 0)
+    "Bad argument in arithmetic expression: second parametr cant be 0"
+
+    iex> Calc.div("15", "3")
+    "Bad argument!"
   """
 
-  @spec div(integer(), integer()) :: float()
-  def div(x, y), do: x / y
+  @spec div(integer(), integer() | float()) :: float()
+  def div(_x, y) when y == 0 do
+    "Bad argument in arithmetic expression: second parametr cant be 0"
+  end
+
+  def div(x, y)
+      when is_integer(x) or
+             is_float(x) or
+             is_integer(y) or
+             is_float(y) do
+    x / y
+  end
+
+  def div(_x, _y), do: "Bad argument!"
 end
--- calc/test/calc_test.exs
+++ calc/test/calc_test.exs
@@ -4,9 +4,45 @@ defmodule CalcTest do
 
   test "addition", do: assert Calc.add(2, 2) == 4
 
+  test "addition with first float", do: assert Calc.add(2.0, 2) === 4.0
+
+  test "addition with second float", do: assert Calc.add(2, 2.0) === 4.0
+
+  test "not valid addition argumnet" do
+    assert Calc.div("2", "2") == "Bad argument!"
+  end
+
   test "subtraction", do: assert Calc.sub(4, 2) == 2
+
+  test "subtraction with first float", do: assert Calc.sub(4.0, 2) === 2.0
+
+  test "subtraction with second float", do: assert Calc.sub(4, 2.0) === 2.0
+
+  test "not valid subtraction argumnet" do
+    assert Calc.div("4", "2") == "Bad argument!"
+  end
 
   test "multiply", do: assert Calc.multi(3, 5) == 15
 
-  test "division", do: assert Calc.div(15, 3) == 5.0
+  test "multiply with first float", do: assert Calc.multi(3.0, 5) === 15.0
+
+  test "multiply with second float", do: assert Calc.multi(3, 5.0) === 15.0
+
+  test "not valid multiply argumnet" do
+    assert Calc.div("3", "5") == "Bad argument!"
+  end
+
+  test "division", do: assert Calc.div(15, 3) == 5
+
+  test "division with first float", do: assert Calc.div(15.0, 3) === 5.0
+
+  test "division with second float", do: assert Calc.div(15, 3.0) === 5.0
+
+  test "division with 0" do
+    assert Calc.div(15, 0) == "Bad argument in arithmetic expression: second parametr cant be 0"
+  end
+
+  test "not valid division argumnet" do
+    assert Calc.div("15", "3") == "Bad argument!"
+  end
 end
