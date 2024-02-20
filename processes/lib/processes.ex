cd3ef599841b5022ca8cc65b38872324bbc92080
Артур Бадретдинов <arthur.badretdinov@gmail.com>
2024-02-20 21:14:44 +0500

    Начальная реализация распределенного приложения

--- processes/lib/processes.ex
+++ processes/lib/processes.ex
@@ -4,15 +4,26 @@ defmodule Processes do
   """
 
   @doc """
-  Hello world.
+  $ NODES=5 iex -S mix
+  стартует 5 нод, к которым можно прицепиться
+  как в примере ниже
 
   ## Examples
 
-      iex> Processes.hello()
-      :world
+      iex> Node.connect(:"node1@localhost")
+      :true
 
   """
-  def hello do
-    :world
+
+  def start_link do
+    nodes = Application.get_env(:processes, Processes)[:nodes]
+    Enum.each(nodes, fn i -> start_node(i) end)
+  end
+
+  defp start_node(%{name: name, type: type}) do
+    case type do
+      :sname -> Node.start(:"#{name}", :shortnames)
+      :name -> Node.start(:"#{name}")
+    end
   end
 end
